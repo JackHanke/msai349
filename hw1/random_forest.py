1b02d38027db902bb63f7e0c92990a9c55964d77
Daniel Plotkin <danielplotkin@Daniels-MacBook-Pro-3.local>
2024-10-17 12:40:40 -0500

    Added tests to random forest

--- hw1/random_forest.py
+++ hw1/random_forest.py
@@ -8,14 +8,14 @@ def random_forest(examples: list[dict[str, any]], n_trees: int = 3, possible_val
 
     for _ in range(n_trees):
         # Get subset using bootstrap sampling
-        example_subset = random.choice(examples, len(examples))
+        example_subset = [random.choice(examples) for _ in range(len(examples))]
         
         # Get attributes to randomly sample a subset
         attributes = list(examples[0].keys())
         attributes.remove('Class')
 
         # Make feature subset
-        feature_subset = random.choice(attributes, feature_subset_size)
+        feature_subset = list(set([random.choice(attributes) for _ in range(feature_subset_size)])) + ['Class']
 
         # Get final subset
         subset = [{key: example[key] for key in feature_subset if key in feature_subset} for example in example_subset]
@@ -48,3 +48,24 @@ def test_random_forest(forest: list[Node], examples: list[dict[str, any]]) -> fl
             total_correct += 1
         # Return accruacy
         return float(total_correct/total_correct)
+    
+if __name__ == '__main__':
+  from parse import parse
+
+  # train_examples = parse('mushroom.data')
+  # examples = parse('tennis.data')
+  train_examples = parse('cars_train.data')
+  
+  model = random_forest(examples=train_examples)
+
+  # Make prediction on a row of data
+  row = train_examples[0]
+  prediction = evalulate_random_forest(forest=model, example=row)
+  print(f'Prediction for row: {row} is equal to {prediction}')
+
+  # Prune tree
+  validation_examples = parse('cars_valid.data')
+
+  # Test again after pruning
+  accuracy = test_random_forest(forest=model, examples=validation_examples)
+  print(f'Accuracy of tree on validation examples = {accuracy:.4f}')
