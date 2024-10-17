62980d7e177b64a349cf89fa5306cae3aecb8237
JackHanke <jack.hanke98@gmail.com>
2024-10-17 11:32:15 -0500

    Full plotting

--- hw1/plot.py
+++ hw1/plot.py
@@ -20,27 +20,36 @@ thing = [i for i in range(n)]
 training_attempts = []
 unpruned_accs = []
 pruned_accs = []
+attempts = 5
 for training_size in range(10,300,10):
     training_attempts.append(training_size)
     unpruned_accs.append(0)
-    for _ in range(100):
+    pruned_accs.append(0)
+    for _ in range(attempts):
         unpruned_acc = 0
-        indexes = np.random.choice(thing, size=training_size)
-        train_examples, test_examples = [], []
+        indexes = np.random.choice(thing, size=training_size//2)
+        validation_indexes = np.random.choice(thing, size=training_size//2)
+        train_examples, validation_examples, test_examples = [], [], []
         for index, row in enumerate(raw_examples):
             if index in indexes: train_examples.append(row)
+            elif index in validation_indexes: validation_examples.append(row)
             else: test_examples.append(row)
 
         root_node = ID3(examples=train_examples, default=0, possible_vals=possible_vals)
         unpruned_acc = test(node=root_node, examples=test_examples)
-    unpruned_accs[-1] = (unpruned_accs[-1] + unpruned_acc)/100
+        unpruned_accs[-1] += ((unpruned_acc)/attempts)
+        print(f'>> An unpruned accuracy for {training_size} = {unpruned_acc}')
 
-        # pruned_acc = 
-        # pruned_accs.append(pruned_acc)
+        pruned_node = prune(node=root_node, examples=validation_examples) # TODO
+        pruned_acc = test(node=pruned_node, examples=test_examples)
+        pruned_accs[-1] += ((pruned_acc)/attempts)
+        print(f'>> A pruned accuracy for {training_size} = {unpruned_acc}')
+
+    print(f'Completed training size {training_size}')
 
 plt.scatter(x=training_attempts, y=unpruned_accs)
+plt.scatter(x=training_attempts, y=pruned_accs)
 plt.xlabel('Training Size')
 plt.ylabel('Accuracy')
-plt.title("house_data_84.data")
-# plt.scatter(x=training_attempts, y=pruned_accs)
+plt.title("Training size for US House 1984 Dataset")
 plt.show()
