a9e7cf54cf19c25a55c1e0e30c7055b5b2a9d5a7
Nicole Birova <nicolebirova2024@u.northwestern.edu>
2024-10-31 15:31:59 -0500

    Update starter.py

--- hw2/starter.py
+++ hw2/starter.py
@@ -154,7 +154,7 @@ we decided that it
 '''
 
 
-def cluster_allignment(query, means=kmeans):
+def cluster_alignment(query, means=kmeans):
     def entropy_calc(arr, base=2): 
         # Helper function to avoid math error for log(0)
         def entropy_term(freq, base):
@@ -217,7 +217,7 @@ def show(file_name,mode):
         print(' ')
 
 #applies PCA for dimensionality reduction on the given train and query datasets
-def apply_pca(train_data, query_data, n_components=2, return_labels=True):
+def apply_pca(train_data, query_data, n_components=50, return_labels=True):
     
     # Extract features and labels from the training data
     train_features = [item[-1] for item in train_data]
@@ -273,8 +273,8 @@ def main(algorithm):
 
         # test alignment with cluster allignment 
         reduced_train_data, reduced_test_data = apply_pca(training_data, validation_data, n_components=50, return_labels=True)
-        quant_metric = cluster_allignment(query=reduced_test_data, means=query_clusters)
-        print(f'Cluster Allighment (entropy) is {quant_metric}')
+        quant_metric = cluster_alignment(query=reduced_test_data, means=query_clusters)
+        print(f'Cluster Alignment (entropy) is {quant_metric}')
 
 if __name__ == "__main__":
     main(algorithm='kmeans')
