430c04abe1e821cdd05f941f7c32f58cfd932a6c
Daniel Plotkin <danielplotkin@Daniels-MBP-6.ht.home>
2024-11-18 09:28:08 -0600

    Removed requirements script

--- project/scripts/update_requirements_file.sh
+++
@@ -1,7 +0,0 @@
-filename='requirements.txt'
-if [ -e "$filename" ]; then
-    echo "Found existing $filename..."
-    rm "$filename"  # Use quotes for safety
-fi
-echo "Creating new $filename..."
-pip freeze > "$filename"  
