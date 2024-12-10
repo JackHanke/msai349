fa5a82eba8f71e0c6ab5598008f0e9a6ea0c0df8
Hanna Zelis <hannazelis@gmail.com>
2024-12-09 21:07:00 -0600

    two plot titles changed eda

--- project/utils/plotting.py
+++ project/utils/plotting.py
@@ -287,7 +287,8 @@ def plot_preprocessed_class_distributions() -> None:
         )
 
     #label for the graph title
-    plt.title("Class Distribution for All Classes", fontsize=16, fontweight="bold")
+    plt.title("Class Distribution for All Classes After Preprocessing", fontsize=16, fontweight="bold")
+
     #label x-axis
     plt.xlabel("Classes", fontsize=14)
     #label y-axis
